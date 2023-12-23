e0e9a936ea3358ad95a65ba2119addde356f2fa1
Julian <149454435+soLojuLianbooLean@users.noreply.github.com>
2023-12-23 15:05:09 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var nowShowing: UIImageView!
     
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-
+    
+    
+    
     func textFieldShouldReturn(_  textField: UISearchBar)  -> Bool {
         searchTextField.endEditing(true)
         return true
@@ -28,41 +30,50 @@ class ViewController: UIViewController {
     struct Movies  {
         let movieURL = "https://7c5l2752slr66y56baikmdzahi0tdlxj.lambda-url.us-east-1.on.aws/"
         
+        func fetchMovie(movieName: String) {
+            let urlString = "\(movieURL)&q=\(movieName)"
+            performRequest(with: urlString) }
         
         
         func performRequest(with urlString: String)  {
             if let url = URL(string: urlString)  {
                 let session = URLSession(configuration: .default)
                 let task = session.dataTask(with: url) { <#Data?#>, <#URLResponse?#>, <#Error?#> in
-                  
-                        return
-                    }
+                    
+                    return
                 }
+                
             }
-       
-        func parseJSON(_ weatherData: Data) -> MovieModel? {
-             let decoder = JSONDecoder()
-             do {
-                 let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-                 let id = decodedData.weather[0].id
-                 let temp = decodedData.main.temp
-                 let name = decodedData.name
-                 
-                 let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
-                 return weather
-                 
-             }
+        }
         
-    }
+        func parseJSON(_ movieData: Data) -> MovieModel? {
+            let decoder = JSONDecoder()
+            do {
+                let decodedData =
+                try JSONDecoder().decode(MovieData.self, from: movieData)
+                let genre = decodedData.genre
+                let name = decodedData.name
+                
+                return   MovieModel.init(conditionId: <#T##Int#>, movieName: <#T##String#>)
+                
+                catch let error {
+                    
+                    self.text = error.localizedDescription
+                    return "Movie could not be found"
+                    
+                    return nil}
+                
+                
+            }
+            
+            
+        } }
         
         struct MovieModel {
             let conditionId: Int
-            let cityName: String
-            let temperature: Double
+            let movieName: String
             
-            var temperatureString: String {
-                return String(format: "%.1f", temperature)
-            }
-
-}
+        }
+        
+    }
 
